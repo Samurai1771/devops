f45a2c19ec5af81a2d81809ab01e59fcc372da17
Test <samurai1771@ya.ru>
2019-12-28 22:15:04 +0300

    Revert "add head"
    This reverts commit af0bbf009c7e8380d18f59fbd9d72038d2cfa39e.

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
 echo "hello world!!!"
+<<<<<<< HEAD
 ls -l /home/
 echo "Im working!"
 <<<<<<< HEAD
@@ -8,3 +7,5 @@ dh -f
 echo "end"
 =======
 >>>>>>> parent of 7cc560c... rename file and change script
+=======
+>>>>>>> parent of af0bbf0... add head
